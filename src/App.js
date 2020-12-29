ab7e021e7c9838c02e4a86694b0ef053a92240f9
Ben Garlock <ben.garlock@gmail.com>
2020-12-29 15:00:43 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import "./stylesheets/App.css"
-import { Route } from 'react-router-dom'
-import About from "./Containers/About";
 import {NavLink} from "react-router-dom";
-
+import backgroundImage1 from "./media/background1.jpg"
 
 
 class App extends React.Component {
+
+    componentDidMount() {
+        const rootWrapper = document.getElementById("root-wrapper")
+        rootWrapper.style.backgroundImage = backgroundImage1
+    }
 
     render() {
         return (
@@ -14,13 +17,16 @@ class App extends React.Component {
                 <div id="header-wrapper">
                     <div id="name">Ben Garlock</div>
                     <NavLink className="menu-select" id="home" to="/">Home</NavLink>
-                    <NavLink className="menu-select" id="about" to="/about">About</NavLink>
+                    <div className="menu-select" id="about" to="/about">About</div>
                     <NavLink className="menu-select" id="portfolio" to="/portfolio">Portfolio</NavLink>
                     <NavLink className="menu-select" id="contact" to="/contact">Contact</NavLink>
+
+                    <div id="hello">
+                        <p style={{fontSize: "100px"}}>Hello. </p>
+                        <p>My name is <span style={{color: "red"}}>Ben Garlock.</span></p>
+                        <p>I'm a full-stack web developer</p>
+                    </div>
                 </div>
-
-                <Route exact path='/about' render={ () => <About /> } />
-
             </div>
         );
     }
