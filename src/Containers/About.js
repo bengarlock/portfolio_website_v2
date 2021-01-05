d6ec9b334f85873fa0ec6904eac75cf01d9e5749
Ben Garlock <ben.garlock@gmail.com>
2021-01-05 13:16:49 -0500

    hourly commit

--- src/Containers/About.js
+++ src/Containers/About.js
@@ -9,7 +9,7 @@ class About extends React.Component {
     render() {
         return(
             <div id="about-wrapper">
-
+                <div className="page-header">- ABOUT -</div>
             </div>
         )
     }
