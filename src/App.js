cfc759eecb31a8f73e84fc3961698737c8f42e93
Ben Garlock <ben.garlock@gmail.com>
2021-06-01 15:32:01 -0400

    added demandbase

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./stylesheets/App.css"
 import Home from "./Containers/Home";
 import About from "./Containers/About";
 import Portfolio from "./Containers/Portfolio";
+import {Route, Link} from "react-router-dom";
 
 import { Element } from 'react-scroll'
 import Navbar from "./Containers/Navbar";
 import Blog from "./Containers/Blog";
 import Contact from "./Containers/Contact";
-
+import Demandbase from "./Containers/Demandbase";
+import {Switch} from "react-router";
 
 
 class App extends React.Component {
@@ -35,21 +37,31 @@ class App extends React.Component {
             <div id="app-wrapper">
                 {this.state.current_page === "home" ? null : <Navbar
                     changePage={this.changePage} currentPage={this.state.current_page}/>}
-                <Element name="home" id="home-wrapper">
-                    <Home changePage={this.changePage} />
-                </Element>
-                <Element name="about">
-                    <About changePage={this.changePage} />
-                </Element>
-                <Element name="portfolio">
-                    <Portfolio changePage={this.changePage} />
-                </Element>
-                <Element name="blog">
-                    <Blog changePage={this.changePage} />
-                </Element>
-                <Element name="contact">
-                    <Contact changePage={this.changePage} />
-                </Element>
+
+                <Switch>
+                    <Route path="/demandbase">
+                        <Demandbase />
+                    </Route>
+                    <Route path="/">
+                        <Element name="home" id="home-wrapper">
+                            <Home changePage={this.changePage} />
+                        </Element>
+                        <Element name="about">
+                            <About changePage={this.changePage} />
+                        </Element>
+                        <Element name="portfolio">
+                            <Portfolio changePage={this.changePage} />
+                        </Element>
+                        <Element name="blog">
+                            <Blog changePage={this.changePage} />
+                        </Element>
+                        <Element name="contact">
+                            <Contact changePage={this.changePage} />
+                        </Element>
+                    </Route>
+
+                </Switch>
+
             </div>
         );
     }
---
+++ src/Containers/Demandbase.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../stylesheets/Demandbase.css"
+
+class Demandbase extends React.Component {
+
+    render() {
+        return (
+            <div className="container">
+                <h1>Demandbase</h1>
+                <img src="https://alchemyinsurance.com/wp-content/uploads/2018/06/lightbulb.jpg" alt="light-bulb" />
+            </div>
+        )
+    }
+}
+
+export default Demandbase
+
